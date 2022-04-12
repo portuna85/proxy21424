d92bf57c34bb22a45534ba3d1c6f1e00c9129058
portuna85 <portuna85@gmail.com>
2022-04-12 16:51:03 +0900

    proxy pattern update

--- src/main/java/hello/proxy/app/v2/OrderServiceV2.java
+++ src/main/java/hello/proxy/app/v2/OrderServiceV2.java
@@ -4,6 +4,7 @@ public class OrderServiceV2 {
 
     private final OrderRepositoryV2 orderRepository;
 
+
     public OrderServiceV2(OrderRepositoryV2 orderRepository) {
         this.orderRepository = orderRepository;
     }
