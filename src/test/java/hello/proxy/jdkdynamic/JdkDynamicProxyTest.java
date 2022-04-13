4b72f9d26ca5d0b034219d3121d2b58d6d7d6ec4
portuna85 <portuna85@gmail.com>
2022-04-13 15:25:11 +0900

    proxy pattern update

--- src/test/java/hello/proxy/jdkdynamic/JdkDynamicProxyTest.java
+++ src/test/java/hello/proxy/jdkdynamic/JdkDynamicProxyTest.java
@@ -20,12 +20,24 @@ public class JdkDynamicProxyTest {
     }
 
     @Test
+    void dynamicAA() {
+        AInterface targetA = new AImpl();
+        TimeInvocationHandler handler = new TimeInvocationHandler(targetA);
+        AInterface proxy = (AInterface) Proxy.newProxyInstance(AInterface.class.getClassLoader(), new Class[]{AInterface.class}, handler);
+
+        proxy.call();
+        log.info("targetAClass = {}", targetA.getClass());
+        log.info("handlerClass = {}", handler.getClass());
+        log.info("proxyClass = {}", proxy.getClass());
+    }
+
+    @Test
     void dynamicB() {
         BInterface target = new BImpl();
         TimeInvocationHandler handler = new TimeInvocationHandler(target);
         BInterface proxy = (BInterface) Proxy.newProxyInstance(BInterface.class.getClassLoader(), new Class[]{BInterface.class}, handler);
         proxy.call();
-        log.info("targetClass={}", target.getClass());
-        log.info("proxyClass={}", proxy.getClass());
+        log.info("targetClass = {}", target.getClass());
+        log.info("proxyClass = {}", proxy.getClass());
     }
 }
